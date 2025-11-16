02295a72635ab9d05cee8bd00059288493949eb9
tom <tom.sabadin2006@gmail.com>
2025-11-16 19:12:57 +0100

    4. vaja + pdfji navodil

--- DN04/DN04.java
+++ DN04/DN04.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 public class DN04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
 
+        int[] v = new int[k];
+        for (int i = 0; i < n; i++) {
+            v[sc.nextInt()]++;
+        }
+
+        long r = 0;
+        for (int i = 0; i < v.length; i++) {
+            int j = k - i;
+            if (j < v.length) {
+                r += (long) v[i] * v[j];
+            }
+        }
+
+        System.out.println(r);
+        sc.close();
     }
 }
